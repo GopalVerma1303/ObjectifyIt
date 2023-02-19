881b15c68ff06eed3030c7aac3101ae9fcf3037f
Gopal Verma <vgopaldps@gmail.com>
2023-02-19 15:42:37 +0530

    added face detection

--- App.tsx
+++ App.tsx
@@ -1,18 +1,105 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
-import React, { useState } from 'react'
-// import { Camera, CameraType } from 'expo-camera';
+import { StyleSheet, Text, View, Dimensions, LogBox, Image } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { Camera } from './Camera';
 import { CameraType } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
 
 const { height, width } = Dimensions.get('window');
-const screenRatio = height / width;
-
-
 const App = () => {
-
+  const [type, setType] = useState(null);
+  const [box, setBox] = useState<{ boxs: { width: any, height: any, x: any, y: any, yawAngle: any, rollAngle: any }, rightEyePosition: { x: any, y: any }, leftEyePosition: { x: any, y: any }, bottomMouthPosition: any }>({
+    boxs: {
+      width: null,
+      height: null,
+      x: null,
+      y: null,
+      yawAngle: null,
+      rollAngle: null
+    },
+    rightEyePosition: { x: null, y: null },
+    leftEyePosition: { x: null, y: null },
+    bottomMouthPosition: null
+  })
+  const cameraRef = useRef(null);
+  const handlerFace = ({ faces }: any) => {
+    if (faces[0]) {
+      console.log(faces);
+      setBox({
+        boxs: {
+          width: faces[0].bounds.size.width,
+          height: faces[0].bounds.size.height,
+          x: faces[0].bounds.origin.x,
+          y: faces[0].bounds.origin.y,
+          yawAngle: faces[0].yawAngle,
+          rollAngle: faces[0].rollAngle
+        },
+        rightEyePosition: faces[0].RIGHT_EYE,
+        leftEyePosition: faces[0].LEFT_EYE,
+        bottomMouthPosition: faces[0].BOTTOM_MOUTH
+      })
+    }
+    else {
+      setBox({
+        boxs: {
+          width: null,
+          height: null,
+          x: null,
+          y: null,
+          yawAngle: null,
+          rollAngle: null
+        },
+        rightEyePosition: { x: null, y: null },
+        leftEyePosition: { x: null, y: null },
+        bottomMouthPosition: null
+      });
+    }
+  }
+  LogBox.ignoreAllLogs(true);
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={CameraType.front} />
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={CameraType.front}
+        captureAudio={false}
+        onFacesDetected={handlerFace}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.none,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
+      />
+      {
+        box && (
+          <>
+            <Image
+              source={require('./assets/glasses.png')}
+              style={{
+                position: 'absolute',
+                top: box.rightEyePosition.y - 510,
+                left: box.leftEyePosition.x - 200,
+                resizeMode: 'contain',
+                width: Math.abs(box.leftEyePosition.x - box.rightEyePosition.x) + 200
+              }}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                top: box.boxs.y,
+                left: box.boxs.x,
+                height: box.boxs.height,
+                width: box.boxs.width,
+                borderWidth: 5,
+                borderColor: 'red',
+                zIndex: 3000
+              }}
+            >
+            </View>
+          </>
+        )
+      }
     </View>
   )
 }
@@ -28,5 +115,5 @@ const styles = StyleSheet.create({
   camera: {
     height: '100%',
     width: '100%'
-  }
+  },
 })
